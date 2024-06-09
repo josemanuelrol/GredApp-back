a5278d451d8d5a2977117bfa86b1017cf0bcb5a8
José Manuel Roldán <127201874+josemanuelrol@users.noreply.github.com>
2024-06-09 17:37:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Clases con los parámetros de configuración de la aplicación.
 class Config(object):
     ENV='development'
-    SECRET_KEY='3412d9394c2cc785e6e09e053ef0f4d78d86004cf002c1945108640a4b8a303f'
-    MONGO_URI='mongodb+srv://user:hum6Vi6lODovK7bY@gredapp.blaorug.mongodb.net/GredApp'
+    SECRET_KEY='clavesecreta'
+    MONGO_URI='databaseURI'
     DEBUG=True
     PORT=5000
     CONSOLE_LOGS=True
@@ -10,7 +10,7 @@ class Config(object):
 
 class ProductionConfig(Config):
     ENV='production'
-    MONGO_URI='mongodb+srv://user:hum6Vi6lODovK7bY@gredapp.blaorug.mongodb.net/GredApp'
+    MONGO_URI='databaseURI'
     DEBUG=False
     CONSOLE_LOGS=False
     CONSOLE_LOGS_LEVEL="INFO"
